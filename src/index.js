5ab45f448b12682064ff5cbe6e2ae3a417b325b7
shiyayonn <ysonconcepcion@gmail.com>
2020-08-31 15:23:01 +0800

    Added folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const express = require("express") // our express server
-const app = express() // generate an app object
-const bodyParser = require("body-parser") // requiring the body-parser
-const PORT = process.env.PORT || 3000 // port that the server is running on => localhost:3000
-app.use(bodyParser.json()) // telling the app that we are going to use json to handle incoming payload
+const express = require("express")
+const app = express() 
+const bodyParser = require("body-parser") 
+const PORT = process.env.PORT || 3000 
+app.use(bodyParser.json()) 
 
 app.get('/',(req,res)=>{
     res.json({"test":"Hello world"})
@@ -10,5 +10,5 @@ app.get('/',(req,res)=>{
 
 app.listen(PORT, () => {
 
-    console.log(`listening on port ${PORT}`) // print this when the server starts
+    console.log(`listening on port ${PORT}`) 
 })
